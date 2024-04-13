b8a08c27267a61d21ee0f7cb0b38e65b3c37c705
errz99 <errz99@gmail.com>
2024-04-13 19:28:40 +0200

    almost ready to rock

--- magz_g4.zig
+++ magz_g4.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 const print = std.debug.print;
 
-pub fn sayHola() void {
-    print("Hola desde magz_g4");
+pub fn sayHola(name: []const u8) void {
+    print("Hola desde magz_g4, {s}\n", .{name});
 }
